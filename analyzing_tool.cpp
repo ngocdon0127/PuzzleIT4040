79ec74faed86c03d3966545fa89d4959b5aa23b2
Ngoc Don <kaitoukid2701@gmail.com>
2015-11-21 21:50:50 +0700

    Test 1000 random situation of 8-Puzzle

--- analyzing_tool.cpp
+++ analyzing_tool.cpp
@@ -12,9 +12,9 @@
 #include "Heuristic.cpp"
 #include "Algorithm.cpp"
 
-#define NUM_HEURISTIC 4
+#define NUM_HEURISTIC 5
 #define START_STATE 0
-#define FINISH_STATE 10
+#define FINISH_STATE 1000
 
 using namespace std;
 
@@ -36,6 +36,7 @@ float aMaxNode[NUM_HEURISTIC] = {0.0};
 float aSumNode[NUM_HEURISTIC] = {0.0};
 int aSumDeep[NUM_HEURISTIC] = {0};
 int aSolved[NUM_HEURISTIC] = {0};
+Node *backup;
 
 double iDAMinTime[NUM_HEURISTIC] = {0.0};
 double iDAMaxTime[NUM_HEURISTIC] = {0.0};
@@ -46,7 +47,6 @@ float iDASumNode[NUM_HEURISTIC] = {0.0};
 int iDASumDeep[NUM_HEURISTIC] = {0};
 int iDASolved[NUM_HEURISTIC] = {0};
 
-int (*function[4])(void);
 
 int generatePuzzle(void);
 int readData(void);
@@ -58,6 +58,7 @@ int key(Node *p);
 int exist(Node *p);
 int mark(Node *p);
 int swap(datatype& x, datatype &y);
+int freeMemory(void);
 //int up(void);
 //int down(void);
 //int left(void);
@@ -65,6 +66,7 @@ int swap(datatype& x, datatype &y);
 
 int main(int argc, char **argv){
 	srand(time(NULL));
+	N = 3;
 //	function[0] = &up;
 //	function[2] = &down;
 //	function[1] = &left;
@@ -91,8 +93,10 @@ int main(int argc, char **argv){
 	for(startState = START_STATE; startState < FINISH_STATE; startState++){
 		do{
 			generatePuzzle();
-			puts("generated");
+//			puts("generated");
 		} while (!checkSolvable(node));
+		backup = newNode();
+		copy(backup, node);
 		FILE *f = fopen("startState.txt", "a");
 		if (!f){
 			return 0;
@@ -106,7 +110,7 @@ int main(int argc, char **argv){
 		}
 		fprintf(f, "\n");
 		fclose(f);
-		for(choice = '4'; choice <= '4'; choice++){
+		for(choice = '1'; choice <= '5'; choice++){
 	//		if (choice == '4')
 	//			continue;
 			
@@ -118,13 +122,13 @@ int main(int argc, char **argv){
 				continue;
 			}
 			Timer ti;
-			init1();
-			//puts("\ninit done");
+			init();
+//			puts("\ninit done");
 			treeSearch();
 			double y = ti.getElapsedTime();
-			fprintf(fresult, "%-3i: heuristic %c :      %-9.0f %-3i %6.4f s | ", startState, choice, numOfNode, deep, y);
+			fprintf(fresult, "%-3i: heuristic %c :      %-9.0f %-3i %10.4f s | ", startState, choice, numOfNode, deep, y);
 			fclose(fresult);
-			printf("done %c of test %i.\n", choice, startState);
+			printf("A*  : done Heuristic %c of test %i.\n", choice, startState);
 			int sttHeuristic = choice - 49;
 			if (deep >= 0){
 				aSolved[sttHeuristic]++;
@@ -142,6 +146,9 @@ int main(int argc, char **argv){
 				aSumNode[sttHeuristic] += numOfNode;
 				aSumDeep[sttHeuristic] += deep;
 			}
+			freeMemory();
+			node = newNode();
+			copy(node, backup);
 			
 			// IDA*
 			numOfNode = 0;
@@ -150,29 +157,33 @@ int main(int argc, char **argv){
 				continue;
 			}
 			Timer ti1;
-			init1();
+			init();
 			//puts("\ninit done");
 			treeSearch1();
 			double y1 = ti1.getElapsedTime();
-			fprintf(fresult, "%-9.0f %-3i %6.4f s\n",numOfNode, deep, y1);
+			fprintf(fresult, "%-9.0f %-3i %10.4f s\n",numOfNode, deep, y1);
 			fclose(fresult);
-			printf("done %c of test %i.\n", choice, startState);
+			printf("IDA*: done Heuristic %c of test %i.\n", choice, startState);
 			if (deep >= 0){
 				iDASolved[sttHeuristic]++;
 				if (startState == 0){
-					iDAMaxTime[sttHeuristic] = iDAMinTime[sttHeuristic] = y;
+					iDAMaxTime[sttHeuristic] = iDAMinTime[sttHeuristic] = y1;
 					iDAMinNode[sttHeuristic] = iDAMaxNode[sttHeuristic] = numOfNode;
 				}
 				else{
-					iDAMaxTime[sttHeuristic] = iDAMaxTime[sttHeuristic] < y ? y : iDAMaxTime[sttHeuristic];
-					iDAMinTime[sttHeuristic] = iDAMinTime[sttHeuristic] > y ? y : iDAMinTime[sttHeuristic];
+					iDAMaxTime[sttHeuristic] = iDAMaxTime[sttHeuristic] < y1 ? y1 : iDAMaxTime[sttHeuristic];
+					iDAMinTime[sttHeuristic] = iDAMinTime[sttHeuristic] > y1 ? y1 : iDAMinTime[sttHeuristic];
 					iDAMaxNode[sttHeuristic] = iDAMaxNode[sttHeuristic] < numOfNode ? numOfNode : iDAMaxNode[sttHeuristic];
 					iDAMinNode[sttHeuristic] = iDAMinNode[sttHeuristic] > numOfNode ? numOfNode : iDAMinNode[sttHeuristic];
 				}
-				iDASumTime[sttHeuristic] += y;
+				iDASumTime[sttHeuristic] += y1;
 				iDASumNode[sttHeuristic] += numOfNode;
 				iDASumDeep[sttHeuristic] += deep;
 			}
+			
+			freeMemory();
+			node = newNode();
+			copy(node, backup);
 		}
 		FILE *fresult = fopen("result.txt", "a");
 		if (!fresult){
@@ -180,6 +191,7 @@ int main(int argc, char **argv){
 		}
 		fprintf(fresult, "\n");
 		fclose(fresult);
+		
 	}
 	system("cls");
 	f = fopen("analysis-8puzzle.txt", "w");
@@ -188,7 +200,7 @@ int main(int argc, char **argv){
 	}
 	puts("Start writing result.");
 	fprintf(f, "%-10s%8s  %8s %10s %10s     %10s  %10s%10s%10s\n", "Heuristic", "Solved", "MinTime", "MaxTime", "AvgTime", "MinNode", "MaxNode", "AvgNode", "AvgDeep");
-	for(int i = 3; i < NUM_HEURISTIC; i++){
+	for(int i = 0; i < NUM_HEURISTIC; i++){
 		fprintf(f, "%8i: ", i + 1);
 		fprintf(f, "%4i/%-4i   ", aSolved[i], FINISH_STATE - START_STATE);
 		fprintf(f, "%-10.4f %-10.4f ", aMinTime[i], aMaxTime[i]);
@@ -198,7 +210,7 @@ int main(int argc, char **argv){
 		fprintf(f, "%5i\n\n", aSumDeep[i] / aSolved[i]);
 	}
 	
-	for(int i = 3; i < NUM_HEURISTIC; i++){
+	for(int i = 0; i < NUM_HEURISTIC; i++){
 		fprintf(f, "%8i: ", i + 1);
 		fprintf(f, "%4i/%-4i   ", iDASolved[i], FINISH_STATE - START_STATE);
 		fprintf(f, "%-10.4f %-10.4f ", iDAMinTime[i], iDAMaxTime[i]);
@@ -207,23 +219,28 @@ int main(int argc, char **argv){
 		fprintf(f, "%10.1f ", iDASumNode[i] / iDASolved[i]);
 		fprintf(f, "%5i\n\n", iDASumDeep[i] / iDASolved[i]);
 	}
-	puts("Start freeing");
+//	puts("Start freeing");
+	freeMemory();
+	
+//	Sleep(1000);
+	puts("writing result successfully.");
+	return 0;
+}
+
+int freeMemory(void){
 	set<Node*>::iterator it = setNode.begin();
 	Node *nodeToFree = NULL;
 	for(; it != setNode.end(); it++){
 		nodeToFree = *it;
-		free(nodeToFree);
+		freeNode(nodeToFree);
 	}
-	puts("End loop");
+//	puts("End loop");
 	listNode.clear();
 	setNode.clear();
-	
-//	Sleep(1000);
-	return 0;
 }
 
 int generatePuzzle(void){
-	node = new Node;
+	node = newNode();
 	int num;
 	int range = N * N;
 	int a[range];
@@ -239,64 +256,8 @@ int generatePuzzle(void){
 			a[num] = 1;
 		}
 	}
-//	print();
 }
 
-int print(void){
-	system("cls");
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < N; j++){
-			printf("%3i", node->cell[i][j]);
-		}
-		puts("");
-	}
-//	printf("%i", countShuffle);
-	getch();
-}
-
-//int up(void){
-//	int x = blank_x(node);
-//	int y = blank_y(node);
-//	if (x == 0){
-//		down();
-//		return 0;
-//	}
-//	swap(node->cell[x - 1][y], node->cell[x][y]);
-//	return 0;
-//}
-//
-//int down(void){
-//	int x = blank_x(node);
-//	int y = blank_y(node);
-//	if (x == N - 1){
-//		up();
-//		return 0;
-//	}
-//	swap(node->cell[x][y], node->cell[x + 1][y]);
-//	return 0;
-//}
-//
-//int left(void){
-//	int x = blank_x(node);
-//	int y = blank_y(node);
-//	if (y == 0){
-//		right();
-//		return 0;
-//	}
-//	swap(node->cell[x][y], node->cell[x][y - 1]);
-//	return 0;
-//}
-//
-//int right(void){
-//	int x = blank_x(node);
-//	int y = blank_y(node);
-//	if (y == N - 1){
-//		left();
-//		return 0;
-//	}
-//	swap(node->cell[x][y], node->cell[x][y + 1]);
-//	return 0;
-//}
 
 int swap(datatype &x, datatype &y){
 	datatype buf = x;
@@ -307,6 +268,7 @@ int swap(datatype &x, datatype &y){
 
 int init(void){
 	node->g = 0;
+	node->f = 0;
 	node->parent = NULL;
 	node->action = 0;
 	calculate(node);
@@ -315,87 +277,4 @@ int init(void){
 	listNode.push_back(node);
 	setNode.clear();
 	setNode.insert(node);
-//	for(int i = N * N - 2; i >=0; i-- ){
-//		sizeAlloc += (int) (i + 1) * pow(N * N, i + 1);
-//	}
-//	printf("size = %i\n", sizeAlloc);
-	//puts("before alloc");
-	//getch();
-//	existArr = (char*) calloc(sizeAlloc + 1, sizeof(char));
-	//puts("after alloc");
-	//getch();
-//	for(int i = 0; i < size + 2; i++)
-//		if (existArr[i] != 0)
-//			puts("det");
-//	puts("cast inside init first:");
-//	existArr[key(node)] = 1;
-	//puts("cast inside init second:");
-	//printf("key node = %i\n", key(node));
-}
-
-int init1(void){
-//	node = new Node;
-//	srand(time(NULL));
-//	datatype *m = &(node->cell[0][0]);
-//	for(int i = 0; i < N * N; i++)
-//		m[i] = i;
-////	for(int i = 0; i < NUMSUFF; i++){
-//////		system("cls");
-//////		for(int j = 0; j < N; j++){
-//////			for(int k = 0; k < N; k++)
-//////				printf("%i ", node->cell[j][k]);
-//////			puts("");
-//////		}
-////		(*function[rand() % 4])();
-////		//Sleep(100);
-////	}
-//
-//	FILE *f = fopen(fn, "r");
-//	fscanf(f, "%i", &numShuffle);
-//	int act;
-//	int a[10000];
-//	for(int i = 0; i < numShuffle; i++){
-//		fscanf(f, "%i", a + i);
-//	}
-//	for(int i = 0; i < numShuffle; i++){
-//		(*function[a[i]])();
-//	}
-//	fclose(f);
-	node->g = 0;
-	node->parent = NULL;
-	node->action = 0;
-	calculate(node);
-	newcutoff = cutoff = 0;
-	listNode.clear();
-	listNode.push_back(node);
-	setNode.clear();
-	setNode.insert(node);
-}
-
-int result(Node *p){
-	if (p->parent == NULL){
-		return 0;
-	}
-	result(p->parent);
-//	switch (p->action){
-//		case UP:
-//			puts("up");
-//			break;
-//		case DOWN:
-//			puts("down");
-//			break;
-//		case LEFT:
-//			puts("left");
-//			break;
-//		case RIGHT:
-//			puts("right");
-//			break;
-//	}
-//	system("cls");
-//	for(int i = 0; i < N; i++){
-//		for(int j = 0; j < N; j++)
-//			printf("%2i ", p->cell[i][j]);
-//		puts("");
-//	}
-//	Sleep(10);
 }
